8bffe72271e69f8a9454d51e9e163bb6419a0d70
sonalk215 <sonalkhare.sk215@gmail.com>
2021-02-08 15:50:05 +1100

    helperFunction

--- catalog-bunnings/src/helperFunctions.js
+++ catalog-bunnings/src/helperFunctions.js
@@ -47,3 +47,17 @@ export const ToastrMessage=(msg, state)=>{
     //       })
     // }
 }
+
+export const ConvertToCSV= objArray =>{
+    let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    let str = '';
+    for (let i = 0; i < array.length; i++) {
+      let line = '';
+      for (let index in array[i]) {
+        if (line != '') line += ','
+        line += array[i][index];
+      }
+      str += line + '\r\n';
+    }
+    return str;
+}
