03a6d48d53761dabba56e23cf32f999f684a9861
sonalk215 <sonalkhare.sk215@gmail.com>
2021-02-08 16:22:59 +1100

    HELPER FUNCTIONS

--- catalog-bunnings/src/helperFunctions.js
+++ catalog-bunnings/src/helperFunctions.js
@@ -1,5 +1,6 @@
 import { toast } from "react-toastify";
 
+//CREATE OBJECT WITH sku AND DESCRIPTION DATA
 export const SKU_Description_Pair=catalog=>{
     let tempCatalog={};
     for(let i=1; i < catalog.length ;i++) {
@@ -9,11 +10,14 @@ export const SKU_Description_Pair=catalog=>{
     return tempCatalog;
 }
 
+//CREATE OBJECT WITH SKU AND BARCODE PAIR
 export const Code_SKU_Pair = data=>({
     "code": data[2],
     "sku": data[1]
 })
 
+
+//RETURN ARRAY WITH BARCODES FROM SOURCES
 export const CreateBarcodesArray=(tempArr)=>{
     let barcodesString='', barcodesArr=[];
     let sortedTempArr= Object.keys(tempArr).sort().reduce((r,k)=> (r[k] = tempArr[k], r), {});
@@ -23,12 +27,11 @@ export const CreateBarcodesArray=(tempArr)=>{
     }
 
     barcodesArr=barcodesString.split(',');
-    //   allBarcodesBArr=allBarcodesBString.split(',');
     barcodesArr.pop();
-    //   allBarcodesBArr.pop();
     return barcodesArr;
 }
 
+//ADD DESCRIPTION TO FINAL ARRAY
 export const Add_Description=(arr, cond, tempCatalog)=>{
     let filtered = arr.filter(e=>e.Source===cond);
     let addNamesArr=filtered.map(elm=>{
@@ -38,6 +41,8 @@ export const Add_Description=(arr, cond, tempCatalog)=>{
     return addNamesArr
 }
 
+
+//SHOW ERROR MESSAGE IN TOASTR
 export const ToastrMessage=(msg, state)=>{
     if(state==='error') {
         return toast.error(msg, {
@@ -50,19 +55,10 @@ export const ToastrMessage=(msg, state)=>{
             progress: undefined,
           })
     }
-    // else if(state==='succes') {
-    //     return toast.success(msg, {
-    //         position: "top-right",
-    //         autoClose: 5000,
-    //         hideProgressBar: false,
-    //         closeOnClick: true,
-    //         pauseOnHover: true,
-    //         draggable: false,
-    //         progress: undefined,
-    //       })
-    // }
 }
 
+
+//CONVERT ARRAY TO CSV
 export const ConvertToCSV= objArray =>{
     let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
     let str = '';
